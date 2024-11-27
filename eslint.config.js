4db96d16f54de138345c023e8797b5be68be74d1
Vladislav <njiuyfqqq123@gmail.com>
2024-11-27 19:01:21 +0200

    updated settings(lint, prett, ts)

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,28 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  },
-  pluginJs.configs.recommended,
-  {
     plugins: {
-      '@typescript-eslint': typescriptEslintPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-  pluginReact.configs.flat.recommended,
-  {
-    ignores: ['node_modules', 'dist', 'build'], // Добавляем сюда папки или файлы для игнорирования
-  },
-];
+)
